c9af5d98880ad77570e68292ece074d125a1132d
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2023-04-27 00:07:09 +0200

    configure purgecss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,54 +1,63 @@
 export default defineNuxtConfig({
-  // TODO: configure purgeCSS
-  ssr: true,
-  css: [
-    "~/assets/css/main.css",
-    "@fortawesome/fontawesome-svg-core/styles.css",
-  ],
+    // TODO: configure purgeCSS
+    ssr: true,
+    css: [
+        "~/assets/css/main.css",
+        "@fortawesome/fontawesome-svg-core/styles.css",
+    ],
 
-
-  modules: [
-    "@nuxt/devtools",
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/fontaine",
-    "@vite-pwa/nuxt",
-    "@nuxt/image-edge",
-    "@nuxtjs/color-mode",
-    "@nuxt/content",
-    "@nuxtjs/html-validator",
-  ],
-  extends: [
-    'nuxt-seo-kit'
-  ],
-  plugins: [
-      { src: "~/plugins/aos", mode: "client" }
-  ],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    modules: [
+        "@nuxt/devtools",
+        "@nuxtjs/tailwindcss",
+        "@nuxtjs/fontaine",
+        "@vite-pwa/nuxt",
+        "@nuxt/image-edge",
+        "@nuxtjs/color-mode",
+        "@nuxt/content",
+        "@nuxtjs/html-validator",
+        'nuxt-purgecss',
+    ],
+    extends: [
+        'nuxt-seo-kit'
+    ],
+    plugins: [
+        { src: "~/plugins/aos", mode: "client" }
+    ],
+    
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+            '@fullhuman/postcss-purgecss': {
+                content: [ './pages/**/*.vue', './layouts/**/*.vue', './components/**/*.vue' ],
+                safelist: [ 'html', 'body' ]
+            }
+        },
     },
-  },
 
-  // Seo kit
-  runtimeConfig: {
-    public: {
-      siteName: "EternalCode.pl",
-      siteDescription: "EternalCode.pl - Open source projects team",
-      language: "en",
-      titleSeparator: '|',
-      trailingSlash: true,
-    }
-  },
+    // Seo kit
+    runtimeConfig: {
+        public: {
+            siteName: "EternalCode.pl",
+            siteDescription: "EternalCode.pl - Open source projects team",
+            language: "en",
+            titleSeparator: '|',
+            trailingSlash: true,
+        }
+    },
 
-  app: {
-    head: {
-      titleTemplate: '%pageTitle %titleSeparator %siteName'
-    }
-  },
+    unhead: {
+        seoOptimise: true
+    },
+
+    app: {
+        head: {
+            titleTemplate: '%pageTitle %titleSeparator %siteName'
+        }
+    },
 
 
-  htmlValidator: {
-    usePrettier: false, // TODO: enable prettier
-  },
+    htmlValidator: {
+        usePrettier: false, // TODO: enable prettier
+    },
 });
