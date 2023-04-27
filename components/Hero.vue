15a53aaa71ff0334308c51d7b5f25b063308aeda
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2023-04-27 02:01:30 +0200

    Update Hero.vue

--- components/Hero.vue
+++ components/Hero.vue
@@ -48,7 +48,7 @@
     </div>
 
     <div aria-hidden="true" class="absolute inset-x-0 -top-40 -z-10 overflow-hidden blur-3xl sm:-top-80">
-      <div class="relative left-[calc(50%+11rem)] aspect-[2155/878] w-[36.125rem] -translate-x-1/2
+      <div class="relative left-[calc(50%+11rem)] aspect-[1155/878] w-[36.125rem] -translate-x-1/2
       bg-gradient-to-tr from-[#00eeff] to-[#002fff] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="
        clip-path: polygon(
             74.1% 44.1%,
