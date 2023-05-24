08d8dd02cd552a60359b69d9de0ee44968205a2b
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-05-25 00:22:09 +0200

    Replace font awesome icon to beautiful heroicons svg
    Signed-off-by: Martin Sulikowski <vLuckyyy.biznes@gmail.com>

--- components/navbar/ThemeSwitchButton.vue
+++ components/navbar/ThemeSwitchButton.vue
@@ -3,29 +3,21 @@
     aria-label="Toggle Dark Mode"
     class="transition-all duration-500 inline-flex h-10 w-10 items-center justify-center rounded-full hover:bg-gray-200 hover:dark:bg-gray-800"
     @click="toggleTheme">
-    <font-awesome-icon
-      v-if="!isDark"
-      :icon="['fas', 'moon']"
-      class="h-6 w-6 text-black"
-      alt="Moon icon"
-      title="Switch to dark mode" />
-    <font-awesome-icon
-      v-else
-      :icon="['fas', 'sun']"
-      class="h-6 w-6 text-white"
-      alt="Sun icon"
-      title="Switch to light mode" />
+    <svg v-if="!isDark" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+    </svg>
+
+    <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 dark:invert">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+    </svg>
+
+
   </button>
 </template>
 
 <script>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
 export default {
   name: "ThemeSwitchButton",
-  components: {
-    FontAwesomeIcon,
-  },
   data() {
     return {
       isDark: false,
