9f294c8a19e944eab75c6d2297908902362a53f5
CitralFlo <wojtas.michal90@gmail.com>
2023-05-25 20:33:50 +0200

    Add clean animation to terminal

--- components/hero/terminal/Terminal.vue
+++ components/hero/terminal/Terminal.vue
@@ -1,69 +1,69 @@
 <template>
   <div
-    id="terminal"
-    class="h-72 w-auto md:flex md:flex-row rounded-lg hidden md:mt-3 lg:mt-0"
-    aria-hidden="true">
+      id="terminal"
+      class="h-72 w-auto md:flex md:flex-row rounded-lg hidden md:mt-3 lg:mt-0"
+      aria-hidden="true">
     <div id="settings" class="h-full w-1/5">
       <div
-        id="on-off-buttons"
-        class="h-6 rounded-tl-lg select-none dark:bg-[#4B5563] bg-[#d8dde6]">
+          id="on-off-buttons"
+          class="h-6 rounded-tl-lg select-none dark:bg-[#4B5563] bg-[#d8dde6]">
         <div class="w-1/3 m-0 flex flex-row pt-1 pl-1">
           <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <g color="red">
-              <circle cx="50" cy="50" r="35" fill="currentcolor" />
+              <circle cx="50" cy="50" r="35" fill="currentcolor"/>
             </g>
           </svg>
 
           <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <g color="orange">
-              <circle cx="50" cy="50" r="35" fill="currentcolor" />
+              <circle cx="50" cy="50" r="35" fill="currentcolor"/>
             </g>
           </svg>
 
           <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <g color="green">
-              <circle cx="50" cy="50" r="35" fill="currentcolor" />
+              <circle cx="50" cy="50" r="35" fill="currentcolor"/>
             </g>
           </svg>
         </div>
       </div>
       <div
-        id="project-settings"
-        class="h-6 flex center flex-row dark:bg-[#374151] bg-[#cfd0d1]">
+          id="project-settings"
+          class="h-6 flex center flex-row dark:bg-[#374151] bg-[#cfd0d1]">
         <a href="https://github.com/EternalCodeTeam/" target="_blank">
           <NuxtImg
-            class="h-4 w-4 m-1 select-none"
-            format="svg"
-            alt="branch icon"
-            aria-label="branch icon"
-            src="/assets/img/hero/branch.svg"
-            width="16"
-            height="16" />
+              class="h-4 w-4 m-1 select-none"
+              format="svg"
+              alt="branch icon"
+              aria-label="branch icon"
+              src="/assets/img/hero/branch.svg"
+              width="16"
+              height="16"/>
         </a>
         <a href="https://github.com/EternalCodeTeam/" target="_blank">
           <NuxtImg
-            class="h-4 w-4 m-1 select-none"
-            format="svg"
-            alt="project icon"
-            aria-label="project icon"
-            src="/assets/img/hero/project.svg"
-            width="16"
-            height="16" />
+              class="h-4 w-4 m-1 select-none"
+              format="svg"
+              alt="project icon"
+              aria-label="project icon"
+              src="/assets/img/hero/project.svg"
+              width="16"
+              height="16"/>
         </a>
         <a href="https://github.com/EternalCodeTeam/" target="_blank">
           <NuxtImg
-            class="h-4 w-4 m-1 select-none"
-            format="svg"
-            alt="github icon"
-            aria-label="github icon"
-            src="/assets/img/hero/github.svg"
-            width="16"
-            height="16" />
+              class="h-4 w-4 m-1 select-none"
+              format="svg"
+              alt="github icon"
+              aria-label="github icon"
+              src="/assets/img/hero/github.svg"
+              width="16"
+              height="16"/>
         </a>
       </div>
       <div
-        id="files"
-        class="h-72 dark:bg-[#4B5563] bg-[#d8dde6] p-2 rounded-bl-lg">
+          id="files"
+          class="h-72 dark:bg-[#4B5563] bg-[#d8dde6] p-2 rounded-bl-lg">
         <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄▄</p>
         <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄▄▄▄▄</p>
         <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄</p>
@@ -81,68 +81,68 @@
     <div id="main-tab" class="h-60 rounded-tr-lg w-4/5">
       <div id="file-section" class="flex">
         <div
-          id="file-1"
-          class="h- w-1/2 dark:bg-[#374151] bg-[#bfbfbf] flex flex-row dark:text-gray-400 pl-2 select-none">
+            id="file-1"
+            class="h- w-1/2 dark:bg-[#374151] bg-[#bfbfbf] flex flex-row dark:text-gray-400 pl-2 select-none">
           <NuxtImg
-            class="h-4 w-4 m-1 select-none"
-            format="svg"
-            alt="project icon"
-            aria-label="project icon"
-            src="/assets/img/hero/project.svg"
-            width="16"
-            height="16" />
+              class="h-4 w-4 m-1 select-none"
+              format="svg"
+              alt="project icon"
+              aria-label="project icon"
+              src="/assets/img/hero/project.svg"
+              width="16"
+              height="16"/>
           <label>EternalCodeWWW</label>
         </div>
         <div
-          id="file-2"
-          class="h-6 w-1/2 dark:bg-[#1F2A37] bg-[#e6e7e8] flex flex-row rounded-tr-lg dark:text-gray-400 pl-2 select-none border-b-2 border-rose-500 dark:border-gray-900">
+            id="file-2"
+            class="h-6 w-1/2 dark:bg-[#1F2A37] bg-[#e6e7e8] flex flex-row rounded-tr-lg dark:text-gray-400 pl-2 select-none border-b-2 border-rose-500 dark:border-gray-900">
           <NuxtImg
-            class="h-4 w-4 m-1 select-none"
-            format="svg"
-            alt="terminal icon"
-            aria-label="terminal icon"
-            src="/assets/img/hero/terminal.svg"
-            width="16"
-            height="16" />
+              class="h-4 w-4 m-1 select-none"
+              format="svg"
+              alt="terminal icon"
+              aria-label="terminal icon"
+              src="/assets/img/hero/terminal.svg"
+              width="16"
+              height="16"/>
           <label>Terminal</label>
         </div>
       </div>
       <div
-        id="file-2-code "
-        class="h-72 sticky inset-x-0 bottom-0 dark:bg-[#1F2A37] bg-[#e6e7e8] pl-2">
+          id="file-2-code "
+          class="h-72 sticky inset-x-0 bottom-0 dark:bg-[#1F2A37] bg-[#e6e7e8] pl-2">
         <label
-          v-for="(line, index) in lines"
-          v-show="index <= currentIndex"
-          :key="index"
-          :class="[
+            v-for="(line, index) in lines"
+            :key="index"
+            :class="[
             'line',
             'inset-x-0 bottom-0',
             'text-sm',
             'font-mono',
             'leading-6',
+            { 'active': index <= currentIndex },
             line.formatting,
           ]">
           <a
-            v-if="line.special"
-            href="https://eternalcode.pl/"
-            target="_blank"
-            >{{ line.line }}</a
-          >
+              v-if="line.special"
+              href="https://eternalcode.pl/"
+              target="_blank"
+          >{{ line.line }}
+          </a>
           <label v-else>
             {{ line.line }}
           </label>
-          <br v-if="line.endLine" />
+          <br v-if="line.endLine"/>
         </label>
       </div>
       <div
-        id="file-settings"
-        class="w-auto h-6 rounded-br-lg dark:bg-[#374151] bg-[#bdbdbd]"></div>
+          id="file-settings"
+          class="w-auto h-6 rounded-br-lg dark:bg-[#374151] bg-[#bdbdbd]"></div>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
+import {ref, onMounted} from "vue";
 import linesAndFormatting from "~/components/hero/terminal/text";
 
 export default {
@@ -154,10 +154,15 @@ export default {
 
     function displayLines() {
       if (currentIndex.value < linesAndFormatting.length) {
-        setTimeout(() => {
+        if (linesAndFormatting[currentIndex.value].endLine) {
+          setTimeout(() => {
+            currentIndex.value++;
+            displayLines();
+          }, delay);
+        } else {
           currentIndex.value++;
           displayLines();
-        }, delay);
+        }
       }
     }
 
@@ -174,4 +179,15 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.line {
+  opacity: 0;
+}
+
+.line.active {
+  opacity: 1;
+  transition: opacity 0.5s ease-in-out;
+}
+
+</style>
