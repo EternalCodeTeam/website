ea7b2065a7064173b74e52119713b86131ae1b91
CitralFlo <wojtas.michal90@gmail.com>
2023-05-29 22:34:44 +0200

    Small fixes

--- components/hero/Hero.vue
+++ components/hero/Hero.vue
@@ -1,49 +1,51 @@
 <template>
   <div class="relative isolate px-6 pt-14 lg:px-8">
-    <PolygonLeft aria-hidden="true" />
+    <PolygonLeft aria-hidden="true"/>
 
     <div
-      class="mx-auto md:w-full lg:grid flex flex-col md:items-center md:justify-center max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0 ">
+        class="mx-auto md:w-full lg:grid flex flex-col md:items-center md:justify-center max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0 ">
       <div class=" lg:mr-auto md:ml-0 place-self-center lg:col-span-7">
         <h1
-          class="mb-4 lg:max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl text-center md:text-left"
-          data-aos="fade-up"
-          data-aos-duration="500">
+            class="mb-4 lg:max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl text-center lg:text-left"
+            data-aos="fade-up"
+            data-aos-duration="500">
           {{ $t("welcome") }}
         </h1>
 
 
         <p
-          class="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl"
-          data-aos="fade-up"
-          data-aos-duration="550">
+            class="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl text-center lg:text-left"
+            data-aos="fade-up"
+            data-aos-duration="550">
           {{ $t("subwelcome") }}
         </p>
+        <div class="flex flex-col sm:flex-row items-center p-4 justify-center lg:block md:p-0 ">
+          <NuxtLink :to="localePath('/#about')">
+            <button
+                :aria-label="$t('firstButton') + ' button'"
+                class="text-black dark:text-white font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2">
+              <Icon name="material-symbols:arrow-forward" class="mb-[0.5px]"/>
+              {{ $t("firstButton") }}
+            </button>
+          </NuxtLink>
 
-        <NuxtLink :to="localePath('/#about')">
-          <button
-            :aria-label="$t('firstButton') + ' button'"
-            class="text-black dark:text-white font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2">
-            <Icon name="material-symbols:arrow-forward" class="mb-[0.5px]" />
-            {{ $t("firstButton") }}
-          </button>
-        </NuxtLink>
+          <NuxtLink :to="localePath('/team')">
+            <button
+                :aria-label="$t('secondButton') + ' button'"
+                class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+              <Icon name="material-symbols:groups" class="mb-[0.5px]"/>
+              {{ $t("secondButton") }}
+            </button>
+          </NuxtLink>
+        </div>
 
-        <NuxtLink :to="localePath('/team')">
-          <button
-            :aria-label="$t('secondButton') + ' button'"
-            class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-            <Icon name="material-symbols:groups" class="mb-[0.5px]" />
-            {{ $t("secondButton") }}
-          </button>
-        </NuxtLink>
       </div>
-
       <div class="lg:col-span-5 lg:block md:mx-auto h-full">
-        <Terminal />
+        <Terminal/>
       </div>
 
-      <PolygonRight aria-hidden="true" />
+      <PolygonRight aria-hidden="true"/>
+
     </div>
   </div>
 </template>
