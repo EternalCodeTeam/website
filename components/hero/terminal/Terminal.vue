600161f2dc3ea5a644a80482287e109136a9149a
CitralFlo <wojtas.michal90@gmail.com>
2023-05-30 23:53:02 +0200

    get rid of .active class, add tailwindcss formating to terminal and use returns in runTerminal() function

--- components/hero/terminal/Terminal.vue
+++ components/hero/terminal/Terminal.vue
@@ -1,18 +1,26 @@
 <template>
   <div
       id="terminal"
-      class=" w-auto md:flex md:flex-row rounded-lg hidden md:mt-3 lg:mt-0"
+      class="hidden w-auto rounded-lg md:mt-3 md:flex md:flex-row lg:mt-0"
       aria-hidden="true">
     <div id="settings" class="h-full w-1/5">
       <div
           id="on-off-buttons"
-          class="h-6 rounded-tl-lg select-none dark:bg-[#4B5563] bg-[#d8dde6]">
-        <div class="group/item w-2/3  flex flex-row pt-0.5 pl-1 space-x-1 md:h-full">
+          class="h-6 select-none rounded-tl-lg bg-[#d8dde6] dark:bg-[#4B5563]">
+        <div
+            class="group/item flex w-2/3 flex-row space-x-1 pl-1 pt-0.5 md:h-full">
+
           <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <g color="red" class="flex items-center justify-center">
               <circle cx="50" cy="50" r="35" fill="currentcolor "/>
-              <text x="50%" y="55%" dominant-baseline="middle" text-anchor="middle"
-                    class="group/edit group-hover/item:visible invisible absolute text-xl" style="font-size: 400%">x
+              <text
+                  x="50%"
+                  y="55%"
+                  dominant-baseline="middle"
+                  text-anchor="middle"
+                  class="group/edit invisible absolute text-xl group-hover/item:visible"
+                  style="font-size: 400%">
+                x
               </text>
             </g>
           </svg>
@@ -20,8 +28,14 @@
           <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <g color="orange" class="flex items-center justify-center">
               <circle cx="50" cy="50" r="35" fill="currentcolor"/>
-              <text x="50%" y="55%" dominant-baseline="middle" text-anchor="middle"
-                    class="group/edit group-hover/item:visible invisible absolute text-xl" style="font-size: 500%">-
+              <text
+                  x="50%"
+                  y="55%"
+                  dominant-baseline="middle"
+                  text-anchor="middle"
+                  class="group/edit invisible absolute text-xl group-hover/item:visible"
+                  style="font-size: 500%">
+                -
               </text>
             </g>
           </svg>
@@ -29,23 +43,32 @@
           <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <g color="green" class="flex items-center justify-center">
               <circle cx="50" cy="50" r="35" fill="currentcolor"/>
-              <text x="69%" y="10%" dominant-baseline="middle" text-anchor="middle"
-                    class="group/edit group-hover/item:visible invisible absolute text-xl rotate-45"
-                    style="font-size: 400%">🡙
+              <text
+                  x="69%"
+                  y="10%"
+                  dominant-baseline="middle"
+                  text-anchor="middle"
+                  class="group/edit invisible absolute rotate-45 text-xl group-hover/item:visible"
+                  style="font-size: 400%">
+                🡙
               </text>
             </g>
           </svg>
-
         </div>
       </div>
       <div
           id="project-settings"
-          class="h-6 flex center flex-row dark:bg-[#374151] bg-[#cfd0d1] justify-between px-3">
+          class="center flex h-6 flex-row justify-between bg-[#cfd0d1] px-3 dark:bg-[#374151]">
         <a href="https://github.com/EternalCodeTeam/" target="_blank">
-          <Icon name="octicon:git-branch" class="mb-[3px] dark:text-slate-500" size="80%" />
+          <Icon
+              name="octicon:git-branch"
+              class="mb-[3px] dark:text-slate-500"
+              size="80%"/>
         </a>
         <a href="https://github.com/EternalCodeTeam/" target="_blank">
-          <Icon name="fluent:projection-screen-16-regular" class="mb-[3px] dark:text-slate-500" />
+          <Icon
+              name="fluent:projection-screen-16-regular"
+              class="mb-[3px] dark:text-slate-500"/>
         </a>
         <a href="https://github.com/EternalCodeTeam/" target="_blank">
           <Icon name="ph:github-logo-fill" class="mb-[4px] pl-1 dark:text-slate-500" size="85%"/>
@@ -53,39 +76,44 @@
       </div>
       <div
           id="files"
-          class="h-80 dark:bg-[#4B5563] bg-[#d8dde6] p-2 rounded-bl-lg">
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄▄</p>
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄▄▄▄▄</p>
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄</p>
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄▄▄▄</p>
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄▄</p>
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄▄▄</p>
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄</p>
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄▄▄▄</p>
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄</p>
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄▄</p>
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄▄</p>
-        <p class="text-sm font-light text-gray-400 select-none">▄▄▄</p>
+          class="h-80 rounded-bl-lg bg-[#d8dde6] p-2 dark:bg-[#4B5563]">
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄</p>
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄▄▄</p>
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄▄</p>
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄</p>
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄</p>
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄▄</p>
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄</p>
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
+        <p class="select-none text-sm font-light text-gray-400">▄▄▄</p>
       </div>
     </div>
-    <div id="main-tab" class="h-full rounded-tr-lg w-4/5 flex flex-col">
+    <div id="main-tab" class="flex h-full w-4/5 flex-col rounded-tr-lg">
       <div id="file-section" class="flex">
         <div
             id="file-1"
-            class="font-Monaco w-1/2 dark:bg-[#374151] bg-[#bfbfbf] flex flex-row dark:text-gray-400 pl-2 select-none">
-          <Icon name="ic:round-folder" class="mr-1 place-self-center dark:text-slate-500" />
+            class="font-Monaco flex w-1/2 select-none flex-row bg-[#bfbfbf] pl-2 dark:bg-[#374151] dark:text-gray-400">
+          <Icon
+              name="ic:round-folder"
+              class="mr-1 place-self-center dark:text-slate-500"/>
           <span>EternalCodeWWW</span>
         </div>
         <div
             id="file-2"
-            class="font-Monaco h-6 w-1/2 dark:bg-[#1F2A37] bg-[#e6e7e8] flex flex-row rounded-tr-lg dark:text-gray-400 pl-2 select-none border-b-2 border-rose-500 dark:border-gray-900 font-mono">
-          <Icon name="ph:terminal-fill" class="place-self-center left-0 dark:text-slate-500 w-5 " size="85%"/>
+            class="font-Monaco flex h-6 w-1/2 select-none flex-row rounded-tr-lg border-b-2 border-rose-500 bg-[#e6e7e8] pl-2 font-mono dark:border-gray-900 dark:bg-[#1F2A37] dark:text-gray-400">
+          <Icon
+              name="ph:terminal-fill"
+              class="left-0 w-5 place-self-center dark:text-slate-500 "
+              size="85%"/>
           <span class="ml-1 mr-auto">Terminal</span>
         </div>
       </div>
       <div
           id="file-2-code "
-          class="min-h-80 h-80 inset-x-0 dark:bg-[#1F2A37] bg-[#e6e7e8] pl-2">
+          class="min-h-80 inset-x-0 h-80 bg-[#e6e7e8] pl-2 dark:bg-[#1F2A37] ">
         <span
             v-for="(line, index) in lines"
             :key="index"
@@ -95,13 +123,14 @@
             'text-sm',
             'font-mono',
             'leading-6',
-            { 'active': index <= currentIndex },
+            'before:opacity-0',
+            'transition',
+            'duration-500 ease-in-out',
+            'opacity-0',
+            { 'opacity-100': index <= currentIndex },
             line.formatting,
           ]">
-          <a
-              v-if="line.special"
-              href="https://eternalcode.pl/"
-              target="_blank"
+          <a v-if="line.special" href="https://eternalcode.pl/" target="_blank"
           >{{ line.line }}
           </a>
           <span v-else>
@@ -109,15 +138,11 @@
           </span>
           <br v-if="line.endLine"/>
         </span>
-
       </div>
       <div
           id="file-settings"
-          class="w-full m-0  h-6 rounded-br-lg dark:bg-[#374151] bg-[#bdbdbd]">
-
-      </div>
+          class="m-0 h-6 w-full rounded-br-lg bg-[#bdbdbd] dark:bg-[#374151]"></div>
     </div>
-
   </div>
 </template>
 
@@ -132,24 +157,22 @@ export default {
     const currentIndex = ref(0);
     const delay = 300; // Delay in milliseconds
 
-    function displayLines() {
-      if (currentIndex.value < linesAndFormatting.length) {
-        if (linesAndFormatting[currentIndex.value].endLine) {
+    function runTerminal() {
+      if(currentIndex.value < lines.length) {
+        if(linesAndFormatting[currentIndex.value].endLine) {
           setTimeout(() => {
             currentIndex.value++;
-            displayLines();
-          }, delay);
+             return runTerminal();
+          }, delay)
         } else {
           currentIndex.value++;
-          displayLines();
+          return runTerminal();
         }
       }
     }
 
     onMounted(() => {
-      setTimeout(() => {
-        displayLines();
-      }, delay);
+      runTerminal();
     });
     return {
       lines,
@@ -161,16 +184,7 @@ export default {
 
 <style scoped>
 
-.line {
-  opacity: 0;
-}
-
-.line.active {
-  opacity: 1;
-  transition: opacity 0.5s ease-in-out;
-}
 .font-Monaco {
   font-family: Monaco, monospace;
 }
-
 </style>
