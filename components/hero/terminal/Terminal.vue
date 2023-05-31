6667f9232f80f44fa0746f5641ea9f4071e506ac
CitralFlo <wojtas.michal90@gmail.com>
2023-05-31 18:16:18 +0200

    Add reactive perspective changer to Terminal

--- components/hero/terminal/Terminal.vue
+++ components/hero/terminal/Terminal.vue
@@ -1,119 +1,121 @@
 <template>
-  <div
-      id="terminal"
-      class="hidden w-auto rounded-lg md:mt-3 md:flex md:flex-row lg:mt-0"
-      aria-hidden="true">
-    <div id="settings" class="h-full w-1/5">
-      <div
-          id="on-off-buttons"
-          class="h-6 select-none rounded-tl-lg bg-[#d8dde6] dark:bg-[#4B5563]">
-        <div
-            class="group/item flex w-2/3 flex-row space-x-1 pl-1 pt-0.5 md:h-full">
 
-          <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-            <g color="red" class="flex items-center justify-center">
-              <circle cx="50" cy="50" r="35" fill="currentcolor "/>
-              <text
-                  x="50%"
-                  y="55%"
-                  dominant-baseline="middle"
-                  text-anchor="middle"
-                  class="group/edit invisible absolute text-xl group-hover/item:visible"
-                  style="font-size: 400%">
-                x
-              </text>
-            </g>
-          </svg>
+    <div class="card" ref="perspective" @mousemove="onMouseMove" @mouseleave="onMouseLeave" >
+      <div
+          id="terminal"
+          class="hidden w-auto rounded-lg md:mt-3 md:flex md:flex-row lg:mt-0 "
+          aria-hidden="true">
+        <div id="settings" class="h-full w-1/5">
+          <div
+              id="on-off-buttons"
+              class="h-6 select-none rounded-tl-lg bg-[#d8dde6] dark:bg-[#4B5563]">
+            <div
+                class="group/item flex w-2/3 flex-row space-x-1 pl-1 pt-0.5 md:h-full">
 
-          <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-            <g color="orange" class="flex items-center justify-center">
-              <circle cx="50" cy="50" r="35" fill="currentcolor"/>
-              <text
-                  x="50%"
-                  y="55%"
-                  dominant-baseline="middle"
-                  text-anchor="middle"
-                  class="group/edit invisible absolute text-xl group-hover/item:visible"
-                  style="font-size: 500%">
-                -
-              </text>
-            </g>
-          </svg>
+              <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g color="red" class="flex items-center justify-center">
+                  <circle cx="50" cy="50" r="35" fill="currentcolor "/>
+                  <text
+                      x="50%"
+                      y="55%"
+                      dominant-baseline="middle"
+                      text-anchor="middle"
+                      class="group/edit invisible absolute text-xl group-hover/item:visible"
+                      style="font-size: 400%">
+                    x
+                  </text>
+                </g>
+              </svg>
 
-          <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-            <g color="green" class="flex items-center justify-center">
-              <circle cx="50" cy="50" r="35" fill="currentcolor"/>
-              <text
-                  x="69%"
-                  y="10%"
-                  dominant-baseline="middle"
-                  text-anchor="middle"
-                  class="group/edit invisible absolute rotate-45 text-xl group-hover/item:visible"
-                  style="font-size: 400%">
-                🡙
-              </text>
-            </g>
-          </svg>
+              <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g color="orange" class="flex items-center justify-center">
+                  <circle cx="50" cy="50" r="35" fill="currentcolor"/>
+                  <text
+                      x="50%"
+                      y="55%"
+                      dominant-baseline="middle"
+                      text-anchor="middle"
+                      class="group/edit invisible absolute text-xl group-hover/item:visible"
+                      style="font-size: 500%">
+                    -
+                  </text>
+                </g>
+              </svg>
+
+              <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g color="green" class="flex items-center justify-center">
+                  <circle cx="50" cy="50" r="35" fill="currentcolor"/>
+                  <text
+                      x="69%"
+                      y="10%"
+                      dominant-baseline="middle"
+                      text-anchor="middle"
+                      class="group/edit invisible absolute rotate-45 text-xl group-hover/item:visible"
+                      style="font-size: 400%">
+                    🡙
+                  </text>
+                </g>
+              </svg>
+            </div>
+          </div>
+          <div
+              id="project-settings"
+              class="center flex h-6 flex-row justify-between bg-[#cfd0d1] px-3 dark:bg-[#374151]">
+            <a href="https://github.com/EternalCodeTeam/" target="_blank">
+              <Icon
+                  name="octicon:git-branch"
+                  class="mb-[3px] dark:text-slate-500"
+                  size="80%"/>
+            </a>
+            <a href="https://github.com/EternalCodeTeam/" target="_blank">
+              <Icon
+                  name="fluent:projection-screen-16-regular"
+                  class="mb-[3px] dark:text-slate-500"/>
+            </a>
+            <a href="https://github.com/EternalCodeTeam/" target="_blank">
+              <Icon name="ph:github-logo-fill" class="mb-[4px] pl-1 dark:text-slate-500" size="85%"/>
+            </a>
+          </div>
+          <div
+              id="files"
+              class="h-80 rounded-bl-lg bg-[#d8dde6] p-2 dark:bg-[#4B5563]">
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄</p>
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄▄▄</p>
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄▄</p>
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄</p>
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄</p>
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄▄</p>
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄</p>
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
+            <p class="select-none text-sm font-light text-gray-400">▄▄▄</p>
+          </div>
         </div>
-      </div>
-      <div
-          id="project-settings"
-          class="center flex h-6 flex-row justify-between bg-[#cfd0d1] px-3 dark:bg-[#374151]">
-        <a href="https://github.com/EternalCodeTeam/" target="_blank">
-          <Icon
-              name="octicon:git-branch"
-              class="mb-[3px] dark:text-slate-500"
-              size="80%"/>
-        </a>
-        <a href="https://github.com/EternalCodeTeam/" target="_blank">
-          <Icon
-              name="fluent:projection-screen-16-regular"
-              class="mb-[3px] dark:text-slate-500"/>
-        </a>
-        <a href="https://github.com/EternalCodeTeam/" target="_blank">
-          <Icon name="ph:github-logo-fill" class="mb-[4px] pl-1 dark:text-slate-500" size="85%"/>
-        </a>
-      </div>
-      <div
-          id="files"
-          class="h-80 rounded-bl-lg bg-[#d8dde6] p-2 dark:bg-[#4B5563]">
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄</p>
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄▄▄</p>
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄▄</p>
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄</p>
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄</p>
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄▄▄</p>
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄▄</p>
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄▄</p>
-        <p class="select-none text-sm font-light text-gray-400">▄▄▄</p>
-      </div>
-    </div>
-    <div id="main-tab" class="flex h-full w-4/5 flex-col rounded-tr-lg">
-      <div id="file-section" class="flex">
-        <div
-            id="file-1"
-            class="font-Monaco flex w-1/2 select-none flex-row bg-[#bfbfbf] pl-2 dark:bg-[#374151] dark:text-gray-400">
-          <Icon
-              name="ic:round-folder"
-              class="mr-1 place-self-center dark:text-slate-500"/>
-          <span>EternalCodeWWW</span>
-        </div>
-        <div
-            id="file-2"
-            class="font-Monaco flex h-6 w-1/2 select-none flex-row rounded-tr-lg border-b-2 border-rose-500 bg-[#e6e7e8] pl-2 font-mono dark:border-gray-900 dark:bg-[#1F2A37] dark:text-gray-400">
-          <Icon
-              name="ph:terminal-fill"
-              class="left-0 w-5 place-self-center dark:text-slate-500 "
-              size="85%"/>
-          <span class="ml-1 mr-auto">Terminal</span>
-        </div>
-      </div>
-      <div
-          id="file-2-code "
-          class="min-h-80 inset-x-0 h-80 bg-[#e6e7e8] pl-2 dark:bg-[#1F2A37] ">
+        <div id="main-tab" class="flex h-full w-4/5 flex-col rounded-tr-lg">
+          <div id="file-section" class="flex">
+            <div
+                id="file-1"
+                class="font-Monaco flex w-1/2 select-none flex-row bg-[#bfbfbf] pl-2 dark:bg-[#374151] dark:text-gray-400">
+              <Icon
+                  name="ic:round-folder"
+                  class="mr-1 place-self-center dark:text-slate-500"/>
+              <span>EternalCodeWWW</span>
+            </div>
+            <div
+                id="file-2"
+                class="font-Monaco flex h-6 w-1/2 select-none flex-row rounded-tr-lg border-b-2 border-rose-500 bg-[#e6e7e8] pl-2 font-mono dark:border-gray-900 dark:bg-[#1F2A37] dark:text-gray-400">
+              <Icon
+                  name="ph:terminal-fill"
+                  class="left-0 w-5 place-self-center dark:text-slate-500 "
+                  size="85%"/>
+              <span class="ml-1 mr-auto">Terminal</span>
+            </div>
+          </div>
+          <div
+              id="file-2-code "
+              class="min-h-80 inset-x-0 h-80 bg-[#e6e7e8] pl-2 dark:bg-[#1F2A37] ">
         <span
             v-for="(line, index) in lines"
             :key="index"
@@ -138,12 +140,15 @@
           </span>
           <br v-if="line.endLine"/>
         </span>
+          </div>
+          <div
+              id="file-settings"
+              class="m-0 h-6 w-full rounded-br-lg bg-[#bdbdbd] dark:bg-[#374151]"></div>
+        </div>
       </div>
-      <div
-          id="file-settings"
-          class="m-0 h-6 w-full rounded-br-lg bg-[#bdbdbd] dark:bg-[#374151]"></div>
     </div>
-  </div>
+
+
 </template>
 
 <script>
@@ -157,12 +162,13 @@ export default {
     const currentIndex = ref(0);
     const delay = 300; // Delay in milliseconds
 
+
     function runTerminal() {
-      if(currentIndex.value < lines.length) {
-        if(linesAndFormatting[currentIndex.value].endLine) {
+      if (currentIndex.value < lines.length) {
+        if (linesAndFormatting[currentIndex.value].endLine) {
           setTimeout(() => {
             currentIndex.value++;
-             return runTerminal();
+            return runTerminal();
           }, delay)
         } else {
           currentIndex.value++;
@@ -171,6 +177,8 @@ export default {
       }
     }
 
+
+
     onMounted(() => {
       runTerminal();
     });
@@ -178,6 +186,23 @@ export default {
       lines,
       currentIndex,
     };
+
+
+  },
+
+  methods: {
+    onMouseMove(event) {
+      const card = this.$refs.perspective;
+      const relX = (event.offsetX + 1) / card.offsetWidth;
+      const relY = (event.offsetY + 1) / card.offsetHeight;
+      const rotY = `rotateY(${(relX - 0.5) * 60 +10 }deg)`;
+      const rotX = `rotateX(${(relY - 0.5) * -60 -4}deg)`;
+      card.style.transform = `perspective(500px) ${rotY} ${rotX}`;
+    },
+    onMouseLeave() {
+      const card = this.$refs.perspective;
+      card.style.transform = (`rotateY(0deg) rotateX(0deg)`);
+    },
   },
 };
 </script>
@@ -186,5 +211,25 @@ export default {
 
 .font-Monaco {
   font-family: Monaco, monospace;
+}
+
+.card-container {
+  width: 100%;
+  height: 100%; /* Adjust the container height as needed */
+  perspective: 1000px; /* Adjust the perspective value for desired 3D effect */
+  display: flex;
+  align-items: center;
+  justify-content: center; /* Adjust the perspective value for desired 3D effect */
+}
+
+.card {
+  width: auto; /* Adjust the card width as needed */
+  height: auto; /* Adjust the card height as needed */
+  transform-style: preserve-3d;
+  transition: transform 0.3s ease;
+}
+
+.card:hover {
+  transform: rotateY(10deg) rotateX(-10deg);
 }
 </style>
