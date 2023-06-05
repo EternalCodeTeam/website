e92e32246f29d2b7d59d0848206977344d523427
Michał Wojtas <80779749+CitralFlo@users.noreply.github.com>
2023-06-05 20:58:02 +0200

    GH-11 Terminal for Hero section (#11)
    * Initial commit
    
    * Add aria-hidden attribute to Terminal, switch <img> to <picture> type. Add dark theme setup.
    
    * Quick fix up
    
    * Switch <picture> to <NuxtIMG /> add aria-labelm hidth, height etc.
    
    * Add responsive terminal design
    
    * Lintfix
    
    Signed-off-by: Martin Sulikowski <vLuckyyy.biznes@gmail.com>
    
    * Merge theme button from develop
    
    Signed-off-by: Martin Sulikowski <vLuckyyy.biznes@gmail.com>
    
    * Add clean animation to terminal
    
    * Switch <label> to <span>
    
    * Add buttons switches on hover, set sizing to better match ios
    
    * Quick fixup for smaller screens
    
    * Merge branch 'develop' into hero-teminal
    
    * Fix reactivity. Add centered position to other elements on smaller screens
    
    * Fix Welcome, change all svg files to <Icon/>. Add theme responsive colors, add font Monaco to "file" names
    
    * Small fixes
    
    * Small fixes
    
    * Small fixes
    
    * get rid of .active class, add tailwindcss formating to terminal and use returns in runTerminal() function
    
    * Add reactive perspective changer to Terminal
    
    * get rid of <style scoped> settings
    
    * small code cleanup
    
    * Reorder via lintfix.
    
    * Add package-lock.json to .gitignore
    
    * Fix width, switch styling add more buttons, make smaller perspective rotation, change svg buttons to div circles
    
    ---------
    
    Signed-off-by: Martin Sulikowski <vLuckyyy.biznes@gmail.com>
    Co-authored-by: Martin Sulikowski <vLuckyyy.biznes@gmail.com>

--- components/hero/Hero.vue
+++ components/hero/Hero.vue
@@ -3,45 +3,44 @@
     <PolygonLeft aria-hidden="true" />
 
     <div
-      class="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0 justify-center">
-      <div class="mr-auto place-self-center lg:col-span-7">
+      class="mx-auto flex max-w-screen-xl flex-col py-8 pl-2 md:w-full md:items-center md:justify-center lg:grid lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0">
+      <div class="place-self-center md:ml-0 lg:col-span-4 lg:mr-auto">
         <h1
-          class="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl"
+          class="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl lg:max-w-2xl lg:text-left xl:text-6xl"
           data-aos="fade-up"
           data-aos-duration="500">
           {{ $t("welcome") }}
         </h1>
 
         <p
-          class="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl"
+          class="mb-6 max-w-2xl text-center font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-left lg:text-xl"
           data-aos="fade-up"
           data-aos-duration="550">
           {{ $t("subwelcome") }}
         </p>
+        <div
+          class="flex flex-col items-center justify-center p-4 sm:flex-row md:p-0 lg:block">
+          <NuxtLink :to="localePath('/#about')">
+            <button
+              :aria-label="$t('firstButton') + ' button'"
+              class="mb-2 mr-2 px-5 py-2.5 text-center text-sm font-medium text-black dark:text-white">
+              <Icon name="material-symbols:arrow-forward" class="mb-[0.5px]" />
+              {{ $t("firstButton") }}
+            </button>
+          </NuxtLink>
 
-        <NuxtLink :to="localePath('/#about')">
-          <button
-            :aria-label="$t('firstButton') + ' button'"
-            class="text-black dark:text-white font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2">
-            <Icon name="material-symbols:arrow-forward" class="mb-[0.5px]" />
-            {{ $t("firstButton") }}
-          </button>
-        </NuxtLink>
-
-        <NuxtLink :to="localePath('/team')">
-          <button
-            :aria-label="$t('secondButton') + ' button'"
-            class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-            <Icon name="material-symbols:groups" class="mb-[0.5px]" />
-            {{ $t("secondButton") }}
-          </button>
-        </NuxtLink>
-      </div>
-
-      <div class="lg:col-span-5 hidden lg:block">
-        <div class="overflow-hidden">
-          <!--          <NuxtImg format="webp" width="400" height="400" src="/assets/img/coding.png" alt="Coding image" />-->
+          <NuxtLink :to="localePath('/team')">
+            <button
+              :aria-label="$t('secondButton') + ' button'"
+              class="mb-2 mr-2 rounded-lg bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80">
+              <Icon name="material-symbols:groups" class="mb-[0.5px]" />
+              {{ $t("secondButton") }}
+            </button>
+          </NuxtLink>
         </div>
+      </div>
+      <div class="lg:col-span-8 w-full lg:block lg:pl-36 lg:pr-0 px-20">
+        <Terminal />
       </div>
 
       <PolygonRight aria-hidden="true" />
@@ -51,4 +50,5 @@
 <script setup>
 import PolygonRight from "~/components/hero/PolygonRight.vue";
 import PolygonLeft from "~/components/hero/PolygonLeft.vue";
+import Terminal from "~/components/hero/terminal/Terminal.vue";
 </script>
