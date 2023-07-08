bf11e386254b144a2beb243df19eea0944d0f3a6
CitralFlo <wojtas.michal90@gmail.com>
2023-07-08 16:08:45 +0200

    Set Vector and Ball class, use vector velocity to control movement of Blops

--- components/Blobs/Blops.vue
+++ components/Blobs/Blops.vue
@@ -1,31 +1,105 @@
 <template>
   <div
     aria-hidden="true"
-    class="absolute inset-x-0 -top-40 -z-10 overflow-hidden blur-3xl sm:-top-80">
+    class="absolute inset-x-0 -top-40 -z-10 overflow-hidden blur-3xl sm:-top-80"
+  >
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 800">
       <defs>
-        <filter id="bbblurry-filter" x="-100%" y="-100%" width="400%" height="400%" filterUnits="objectBoundingBox"
-                primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-          <feGaussianBlur stdDeviation="40" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" edgeMode="none"
-                          result="blur">
-          </feGaussianBlur>
+        <filter
+          id="bbblurry-filter"
+          x="-100%"
+          y="-100%"
+          width="400%"
+          height="900%"
+          filterUnits="objectBoundingBox"
+          primitiveUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feGaussianBlur
+            stdDeviation="40"
+            x="0%"
+            y="0%"
+            width="100%"
+            height="100%"
+            in="SourceGraphic"
+            edgeMode="none"
+            result="blur"
+          ></feGaussianBlur>
         </filter>
       </defs>
-      <g filter="url(#bbblurry-filter)" class="light:opacity-150 dark:opacity-70">
-        <ellipse v-for="(data, index) in positionsRangeData"
-                 :key="index"
-                 :rx="data.range"
-                 :ry="data.range"
-                 :cx="data.posX"
-                 :cy="data.posY"
-                 fill="hsla(212, 72%, 59%, 1.00)">
-        </ellipse>
+      <g
+        filter="url(#bbblurry-filter)"
+        class="light:opacity-150 dark:opacity-70"
+      >
+        <ellipse
+          v-for="(data, index) in Balls"
+          :key="index"
+          :rx="30"
+          :ry="30"
+          :cx="data.position.x"
+          :cy="data.position.y"
+          fill="hsla(212, 72%, 59%, 1.00)"
+        ></ellipse>
       </g>
     </svg>
   </div>
 </template>
+
 <script>
 import { defineComponent } from 'vue';
+
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(vector) {
+    return new Vector(this.x + vector.x, this.y + vector.y);
+  }
+
+  multiply(scalar) {
+    return new Vector(this.x * scalar, this.y * scalar);
+  }
+
+  subtract(vector) {
+    return new Vector(this.x - vector.x, this.y - vector.y);
+  }
+}
+
+class Ball {
+  constructor(posX, posY, range) {
+    this.position = new Vector(posX, posY);
+    this.velocity = this.generateRandomVelocity(range).multiply(0.2);
+  }
+
+  generateRandomVelocity(range) {
+    const randomIndex = Math.floor(Math.random() * range.length);
+    const randomRange = range[randomIndex];
+
+    const randomX =
+      Math.floor(Math.random() * (randomRange.maxX - randomRange.minX + 1)) +
+      randomRange.minX;
+    const randomY =
+      Math.floor(Math.random() * (randomRange.maxY - randomRange.minY + 1)) +
+      randomRange.minY;
+
+    return new Vector(randomX, randomY);
+  }
+
+  move() {
+    const newPosition = this.position.add(this.velocity);
+    const smoothness = 0.05;
+    this.position = this.position
+      .add(newPosition.subtract(this.position).multiply(smoothness));
+
+    // If the ball is out of the screen, generate a new velocity vector with reverse direction
+    if ( this.position.x < 0 || this.position.x > 800 || this.position.y < 0 || this.position.y > 800 ) {
+      this.velocity = this.velocity.multiply(-1);
+    }
+
+  }
+}
 
 const range = [
   { minX: 50, maxX: 150, minY: 50, maxY: 150 },
@@ -38,56 +112,40 @@ const range = [
   { minX: 650, maxX: 750, minY: 600, maxY: 700 },
 ];
 
+const Balls = [
+  new Ball(50, 50, range),
+  new Ball(10, 680, range),
+  new Ball(30, 450, range),
+  new Ball(80, 50, range),
+  new Ball(150, 600, range),
+  new Ball(500, 50, range),
+  new Ball(650, 400, range),
+  new Ball(650, 600, range),
+];
+
 export default defineComponent({
   name: 'Blops',
   data() {
     return {
-      positionsRangeData: [
-        { posX: 50, posY: 50, range: 80 },
-        { posX: 10, posY: 680, range: 80 },
-        { posX: 30, posY: 450, range: 80 },
-        { posX: 80, posY: 50, range: 80 },
-        { posX: 150, posY: 600, range: 80 },
-        { posX: 500, posY: 50, range: 80 },
-        { posX: 650, posY: 400, range: 80 },
-        { posX: 650, posY: 600, range: 80 },
-      ],
+      Balls,
     };
   },
-  created() {
+  mounted() {
+    // Move the balls gradually
     setInterval(() => {
-      this.moveBall();
-    }, 1000);
-  },
-  methods: {
-    moveBall() {
-      for (let i = 0; i < this.positionsRangeData.length; i++) {
-        const targetX = Math.floor(Math.random() * (range[i].maxX - range[i].minX + 1)) + range[i].minX;
-        const targetY = Math.floor(Math.random() * (range[i].maxY - range[i].minY + 1)) + range[i].minY;
-        const stepX = (targetX - this.positionsRangeData[i].posX) / 30;
-        const stepY = (targetY - this.positionsRangeData[i].posY) / 30;
-
-        const animate = () => {
-          if (this.positionsRangeData[i].posX !== targetX || this.positionsRangeData[i].posY !== targetY) {
-            this.positionsRangeData[i].posX += stepX;
-            this.positionsRangeData[i].posY += stepY;
-
-            // client side only
-            if (process.client) {
-              window.requestAnimationFrame(animate);
-            }
-          }
-        };
-
-        animate();
+      for (const ball of this.Balls) {
+        ball.move();
       }
-    },
+    }, 50);
+    // Generate new random velocity for the balls
+    setInterval(() => {
+      for (const ball of this.Balls) {
+        ball.velocity = ball.generateRandomVelocity(range).multiply(0.2);
+      }
+    }, 5000);
   },
 });
 </script>
+
 <style scoped>
-
 </style>
-
-
-
