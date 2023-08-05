90498e574350223cd96bb4af9c6b11259441aaab
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-08-05 16:42:06 +0200

    start of remake about section.

--- components/About.vue
+++ components/About.vue
@@ -4,8 +4,8 @@
       <div class="items-center gap-x-12 sm:px-4 md:px-0 lg:flex">
         <div class="flex-1 sm:hidden lg:block">
           <NuxtImg
-            src="https://www.eternalcode.pl/assets/img/header/about/eternalcore-intellij.webp"
-            class="rounded-[12px]"
+            src="https://i.imgur.com/CLmOMVd.png"
+            class="rounded-[12px] blur-[1px]"
             alt="about me presentation"
             data-aos="fade-up"
             data-aos-duration="450" />
@@ -31,38 +31,6 @@
             data-aos-duration="550">
             {{ $t("about.text2") }}
           </p>
-
-          <ul class="flex gap-y-6 flex-wrap items-center justify-center">
-            <li class="flex-none" data-aos="fade-up" data-aos-duration="600">
-              <a href="https://github.com">
-                <Icon
-                  name="akar-icons:github-fill"
-                  size="48"
-                  class="m-4 text-black dark:text-white" />
-              </a>
-            </li>
-
-            <li class="flex-none" data-aos="fade-up" data-aos-duration="650">
-              <a href="https://www.jetbrains.com">
-                <Icon name="logos:jetbrains" size="48" class="m-4" />
-              </a>
-            </li>
-
-            <li class="flex-none" data-aos="fade-up" data-aos-duration="700">
-              <a href="https://sentry.io">
-                <Icon
-                  name="vscode-icons:file-type-sentry"
-                  size="48"
-                  class="m-4" />
-              </a>
-            </li>
-
-            <li class="flex-none" data-aos="fade-up" data-aos-duration="750">
-              <a href="https://www.atlassian.com/software/jira">
-                <Icon name="logos:jira" size="48" class="m-4" />
-              </a>
-            </li>
-          </ul>
         </div>
       </div>
     </div>
