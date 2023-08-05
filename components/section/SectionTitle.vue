27c7c01e42b3934bba37cd873073e93bc3ea5be9
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-08-05 16:17:00 +0200

    Update SectionTitle.vue

--- components/section/SectionTitle.vue
+++ components/section/SectionTitle.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="mx-auto mb-8 lg:mb-16">
-    <div class="flex flex-col lg:flex-row justify-between items-center text-center">
+    <div class="flex flex-col lg:flex-row justify-between items-center">
       <div class="lg:w-1/2">
         <h2
-          class="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white mb-4 lg:mb-0 lg:mr-8">
+          class="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white mb-4 lg:mb-0 lg:mr-8 break-words text-center lg:text-left">
           {{ title }}
         </h2>
       </div>
