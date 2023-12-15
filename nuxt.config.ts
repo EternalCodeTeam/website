e258ddd27deae707cf7143be6c8759e201213d9b
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-12-15 18:01:00 +0100

    Resolve seo meta issues.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,6 +58,8 @@ export default defineNuxtConfig({
   app: {
     head: {
       titleTemplate: "%pageTitle %titleSeparator %siteName",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
     },
   },
 });
