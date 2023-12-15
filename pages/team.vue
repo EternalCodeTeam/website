f42ef48b665ed6bd99014e50d068af2f1c53d9a2
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-12-16 00:37:17 +0100

    GH-58 Resolve seo meta issues. (#58)
    * Resolve seo meta issues.
    
    * nuxtseo.
    
    * nuxtseo.

--- pages/team.vue
+++ pages/team.vue
@@ -1,8 +1,26 @@
-<script lang="ts" setup>
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const route = useRoute();
-</script>
-
 <template>
   <LazyTeam />
 </template>
+
+<script lang="ts" setup>
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const route = useRoute();
+
+useSeoMeta({
+  ogImage: "/assets/img/logo-white-background.webp",
+  twitterTitle: "EternalCode.pl",
+  twitterDescription: "We are a team creating open source projects!",
+  twitterImage: "/assets/img/logo-white-background.webp",
+  twitterCard: "We are a team creating open source projects!",
+});
+
+useHead({
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.png",
+    },
+  ],
+});
+</script>
