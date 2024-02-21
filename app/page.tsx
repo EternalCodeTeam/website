ee6cd98cfc1dcdacb4865424c81a188359ce7e82
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-02-21 21:36:24 +0100

    Add cache and force static to root route.
    Signed-off-by: Martin Sulikowski <vLuckyyy.biznes@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Faq from "@/components/faq/Faq";
 import Features from "@/components/features/Features";
 
 export const dynamic = "force-static";
+export const fetchCache = "force-cache";
+
+export const dynamic = "force-static";
 export default function Home() {
   return (
     <main>
