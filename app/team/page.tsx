d98370fe0743ef1b5e8eb2ac7062545f8b72f3f1
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-05-19 15:21:50 +0200

    Remove force propert from team page.

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,9 +1,6 @@
 import Team from "@/components/team/Team";
 import type { Metadata } from "next";
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-cache";
-
 export const metadata: Metadata = {
   title: "EternalCode.pl | Team",
   description: "See our team members and their roles in our projects",
