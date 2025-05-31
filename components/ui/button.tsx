7fb03dfa3fc32a1beb081b11a719eb98642fd5dc
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2025-05-31 18:22:01 +0200

    Remove ghost button
    Signed-off-by: Martin Sulikowski <vLuckyyy.biznes@gmail.com>

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -46,7 +46,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       primary: "bg-blue-600 text-white hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-800",
       secondary: "bg-gray-200 text-gray-900 hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600",
       outline: "border border-gray-300 bg-transparent hover:bg-gray-100 dark:border-gray-600 dark:hover:bg-gray-800",
-      ghost: "bg-transparent hover:bg-gray-100 dark:hover:bg-gray-800",
+      ghost: "bg-transparent dark:bg-transparent",
       link: "bg-transparent text-blue-600 underline-offset-4 hover:underline dark:text-blue-400",
       danger: "bg-red-600 text-white hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-800",
     };
