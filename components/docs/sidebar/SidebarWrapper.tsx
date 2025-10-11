274fc034b937401c0ebe8ca6b34bc66f744da581
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2025-10-11 20:42:09 +0200

    Refactor and enhance docs sidebar UI/UX

--- components/docs/sidebar/SidebarWrapper.tsx
+++ components/docs/sidebar/SidebarWrapper.tsx
@@ -7,15 +7,17 @@ const SidebarWrapper: FC = () => {
   return (
     <>
       {/* Desktop sidebar */}
-      <aside className="hidden w-full flex-shrink-0 lg:block lg:w-64">
-        <div className="sticky top-32 z-20 flex flex-col gap-4">
+      <aside className="hidden w-72 flex-shrink-0 lg:block">
+        <div className="sticky top-32 flex h-[calc(100vh-8rem)] flex-col gap-4">
           <DocsSearch />
-          <DocSidebar />
+          <div className="flex min-h-0 flex-1 flex-col overflow-hidden rounded-xl border border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-900">
+            <DocSidebar />
+          </div>
         </div>
       </aside>
 
       {/* Mobile sidebar */}
-      <div className="lg:hidden flex flex-col gap-4">
+      <div className="flex flex-col gap-4 lg:hidden">
         <DocsSearch />
         <DocSidebar />
       </div>
